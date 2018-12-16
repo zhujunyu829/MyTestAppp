fc45fb29d1eca5bd7c10e71bac10aef9d49b09b9
朱俊宇 <171959017@qq.com>
2018-12-16 09:31:00 +0800

    提交项目
    项目提交

---
+++ SaleApp/SaleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SaleApp
+//
+//  Created by zjy on 2018/12/16.
+//  Copyright © 2018年 hechangqiye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SaleApp/SaleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SaleApp
+//
+//  Created by zjy on 2018/12/16.
+//  Copyright © 2018年 hechangqiye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
